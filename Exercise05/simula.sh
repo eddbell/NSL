95b8c688608bb67218ab7c77335d0f8f1e0a5ca2
Edoardo Bella <eddbell97@gmail.com>
2023-10-09 13:49:35 +0200

    final commit

--- Exercise05/simula.sh
+++ Exercise05/simula.sh
@@ -41,32 +41,32 @@ done
 rm -rf output_*
 
 echo -e "_________Block statistics analysis_________\n\n"
-for steps in 1000 10000 100000
-do
-	cp input.dummy input.in
-	sed -i "s/iD_dummy/1/g" input.in
-	sed -i "s/iGS_dummy/0/g" input.in
-	sed -i "s/delta_dummy/0.8/g" input.in
-	sed -i "s/x_dummy/1/g" input.in
-	sed -i "s/steps_dummy/${steps}/g" input.in
-	sed -i "s/nblk_dummy/100/g" input.in
+N=100000
+M=100000
+cp input.dummy input.in
+sed -i "s/iD_dummy/1/g" input.in
+sed -i "s/iGS_dummy/0/g" input.in
+sed -i "s/delta_dummy/0.8/g" input.in
+sed -i "s/x_dummy/1/g" input.in
+sed -i "s/steps_dummy/$N/g" input.in
+sed -i "s/nblk_dummy/$M/g" input.in
 
-	echo -e "<PARAMETERS INFO>"
-	echo -e "Steps =        ${steps}"
-	echo -e "Blocks =       100             (FIXED)"
-	echo -e "iD =           1               (FIXED)"
-	echo -e "iGS =          0               (FIXED)"
-	echo -e "delta =        0.8             (FIXED)"
-	echo -e "position (x) = 1               (FIXED)"
+echo -e "<PARAMETERS INFO>"
+echo -e "Steps =        $N"
+echo -e "Blocks =       $M              (FIXED)"
+echo -e "iD =           1               (FIXED)"
+echo -e "iGS =          0               (FIXED)"
+echo -e "delta =        0.8             (FIXED)"
+echo -e "position (x) = 1               (FIXED)"
 
-	./main.exe
+./main.exe
 
-	mv output_rad.dat ./data/output_rad_stat_${steps}.dat
-	mv output_pos.dat ./data/output_pos_stat_${steps}.dat
-	echo "<INFO> Saving position output file in: ./data/output_rad_stat_${steps}.dat"
-  echo "<INFO> Saving radius output file in:   ./data/output_pos_stat_${steps}.dat"
-  echo -e "______________________________________________________________________\n\n"
-done
+mv output_rad.dat ./data/output_rad_equilibration.dat
+mv output_pos.dat ./data/output_pos_equilibration.dat
+echo "<INFO> Saving position output file in: ./data/output_rad_equilibration.dat"
+echo "<INFO> Saving radius output file in:   ./data/output_pos_equilibration.dat"
+echo -e "______________________________________________________________________\n\n"
+
 rm -rf output_*
 
 delta=(2.5 6 0.8 1.8)
